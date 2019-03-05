8b25093f3d2f648aa419f4f82624cf4d5d1970e2
walker911 <qinmu911@163.com>
2019-03-05 23:16:07 +0800

    feat: add home and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RouterModule, Routes} from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home'},
+  { path: 'home', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
