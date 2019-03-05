ae06de818850f32076738d745991f3e21c5cc2ce
walker911 <qinmu911@163.com>
2019-03-05 23:17:02 +0800

    feat: update home component

---
+++ src/app/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+    templateUrl: './home.component.html'
+})
+export class HomeComponent {
+
+    title = 'demo';
+    greeting = {};
+
+    constructor(private app: AppService, private http: HttpClient) {
+        http.get('').subscribe(data => this.greeting = data);
+    }
+
+    authenticated() {
+        return this.app.authenticated;
+    }
+}
