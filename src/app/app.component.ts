506b3c04b54f8cda9eb8cd20a09fcc837223f54e
walker911 <qinmu911@163.com>
2019-03-06 23:24:57 +0800

    feat: update app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AppService } from './app.service';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +10,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'security-front';
-  greeting = {};
-  constructor(private http: HttpClient) {
-    http.get('api/greeting').subscribe(data => this.greeting = data);
+  constructor(private app: AppService, private http: HttpClient, private router: Router) {
+    this.app.authenticate(undefined, undefined);
+  }
+
+  logout() {
+    this.http.post('logout', {}).pipe(finalize(() => {
+      this.app.authenticated = false;
+      this.router.navigateByUrl('/login');
+    })).subscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule, Routes} from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 import { HomeComponent } from './home.component';
@@ -26,7 +26,8 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
